9e438fb385ab1f50ef4010ca186726aef83af096
Eric Liang <ekhliang@gmail.com>
2010-05-04 15:02:33 -0700

    tweak some constants

--- config.h
+++ config.h
@@ -7,11 +7,17 @@
 
 #define HASHMAP_DEFAULT_CAPACITY 100003
 #define QUEUE_DEFAULT_CAPACITY 100
-#define QUEUE_DELAY 80
-#define PARALLEL 0
-#define PARALLEL_PROBLEM_SIZE_THRESHOLD 5
+
+#define PARALLEL 1
+#define PARALLEL_PROBLEM_SIZE_THRESHOLD 20
 #define PARALLEL_PROBLEM_MAX_THREADS 2
-#define PARALLEL_THREAD_LOCAL_QUEUES 1
+
+// set higher to increase search breadth
+#define QUEUE_DELAY 80
+
+// these two tend to conflict with each other
+// resulting in highly unstable times for certain puzzles
+#define PARALLEL_THREAD_LOCAL_QUEUES 0
 #define SHUFFLE_NEW_STATES 1
 
 #define SCORE_UNDEFINED_SCORE 999999
