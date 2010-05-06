e1d5ae63ff80f06cfd640b388bfb978bcd593b1f
Eric Liang <ericliang@berkeley.edu>
2010-05-06 03:16:34 -0700

    remove worst test

--- worst_case.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "This takes a long time, which is silly given that the state
-1 move previous is trivially findable."
-
-time (
-	./ice data/start2.pbm data/end2-longtime.pbm &&
-	echo "DONE"
-)
